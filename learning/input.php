67fd6fd563ebde66770bf31fcc602479602f5946
Javier Salas <JavierTSalas@users.noreply.github.com>
2017-06-08 20:28:05 -0400

    Added request.log and vardump.log
    request.log is a file that write the contents of the $_REQUEST string
    vardump.log is a parsed version of the contents of the $_REQUEST string

--- learning/input.php
+++ learning/input.php
@@ -4,10 +4,36 @@ include 'connect.php';
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 	inputAssignments ();
 } else {
+	$testing = "Array
+(
+    [Assignments] => Array
+        (
+            [0] => Array
+                (
+                    [ass_name] => Test123
+                    [class_id] => 2
+                    [date_assigned] => 2017-08-23
+                    [done] => false
+                    [due] => 2017-08-23 13:34:54
+                    [id] => 10
+                    [weight] => 65
+                )
+
+        )
+
+)
+";
+	$json = json_decode($testing,true);
+	echo $json;
 	echo "This page is ment to be called with a post header </br>";
 }
 function inputAssignments() {
 
+	$req_dump = print_r( $_REQUEST, true );
+	$dump = json_decode($req_dump, true);
+	$fp = file_put_contents( 'request.log', $req_dump );
+	$fp = file_put_contents( 'vardump.log', $dump);
+	
 	
 	
 	echo "inputAssignments";
